dee8bde1aea0a6e31b68ccff8b609fc7e68af198
KrishBharadwaj5679 <krishbharadwaj43@gmail.com>
2026-09-14 09:10:37 +0530

    Add favicons, Improve Styling

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express')
-const app = express()
-app.use(express.static('public')) //serve our files in public statically
-app.listen(5000)
+const express = require("express");
+const app = express();
+app.use(express.static("public")); //serve our files in public statically
+app.listen(5000);
+console.log("Server is running on port 5000...");
